45a438bf6e92bb8a4c708003c890a6fee85cef77
davi-rdgr <davirdgrsouza@gmail.com>
2024-04-05 17:07:35 -0300

    Estilização do modal + reset no App css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav.jsx'
 import Footer from './components/Footer.jsx'
+import styles from "./css/App.module.css"
 export default function App() {
   return (
     <>
--- src/components/ModalActions.jsx
+++ src/components/ModalActions.jsx
@@ -1,10 +1,11 @@
+import styles from '../css/ModalActions.module.css'
 export default function ModalActions(props) {
     if (props.open) {
         return (
-            <>
+            <div className={styles.container}>
                 <h1>MODAL</h1>
                 <button onClick={() => props.setOpen(false)}>CLOSE MODAL</button>
-            </>
+            </div>
         )
     }
 }
