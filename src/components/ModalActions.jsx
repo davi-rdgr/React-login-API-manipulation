6778c1bd947d26a05fc2752e05496c8d5caaf295
davi-rdgr <davirdgrsouza@gmail.com>
2024-04-05 17:14:40 -0300

    Form edit criado

--- src/components/ModalActions.jsx
+++ src/components/ModalActions.jsx
@@ -3,8 +3,35 @@ export default function ModalActions(props) {
     if (props.open) {
         return (
             <div className={styles.container}>
-                <h1>MODAL</h1>
+                <h1>Produtos</h1>
                 <button onClick={() => props.setOpen(false)}>CLOSE MODAL</button>
+
+                <div>
+                    <form>
+                        <fieldset>
+                            <legend>Dados do Produto</legend>
+                            <div>
+                                <label htmlFor="idNome">Nome</label>
+                                <input type="text" name="nome" id="idNome" />
+
+                                <label htmlFor="idDesc">Descrição</label>
+                                <input type="text" name="desc" id="idDesc" />
+
+                                <label htmlFor="idPreco">Preço</label>
+                                <input type="text" name="preco" id="idPreco" />
+
+                                <div>
+                                    <button>Editar</button>
+                                </div>
+
+
+
+
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
             </div>
         )
     }
